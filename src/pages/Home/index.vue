f941106ca5641ba59dbe3426a7069b1adab14835
Westbeauty <1136676649@qq.com>
2021-12-10 21:31:50 +0800

    第二次提交，home组件已拆分完毕

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,12 +2,26 @@
     <div>
         <!-- 全局组件不需要引入了 -->
         <TypeNav/>
+        <ListContainer/>
+        <TodayRecommend/>
+        <Rank/>
+        <Like/>
+        <Floor/>
+        <Floor/>
+        <Brand/>
     </div>
 </template>
 
 <script>
+import ListContainer from './ListContainer/index.vue'
+import TodayRecommend from './TodayRecommend/index.vue'
+import Rank from './Rank/index.vue'
+import Like from './Like/index.vue'
+import Floor from './Floor/index.vue'
+import Brand from './Brand/index.vue'
 export default {
     name:'Home',
+    components:{ListContainer,TodayRecommend,Rank,Like,Floor,Brand}
 
 }
 </script>
